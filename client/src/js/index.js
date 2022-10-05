be2f0073cce5d3c8f8f0d360ce1faea5a124d8fe
bpiper91 <bpiper91@gmail.com>
2022-10-04 22:00:31 -0400

    added stylesheets and bootstrap css to webpack bundle

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,16 @@
+// import modules
 import  "./form";
 import "./submit";
+
+// import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+
+// import CSS
+import "../css/index.css";
+import { Tooltip, Toast, Popper } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
