52ec54fd7a50ba13fe0340f599ce20dca6742a8f
bpiper91 <bpiper91@gmail.com>
2022-10-06 00:08:14 -0400

    added GET and POST routes for idexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,9 @@
 // import modules
 import  "./form";
 import "./submit";
-import { initdb } from './database';
+
+// import database functions
+import { initdb, getDb, postDb } from './database';
 
 // import images
 import Logo from '../images/logo.png';
@@ -15,6 +17,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.addEventListener('load', function () {
     initdb();
+
+    // temporarily placing getDb() and postDb() calls here for testing
+    getDb();
+    postDb("Lernantino", "lernantino@test.com", 8186601234, "Bear");
+    getDb();
+
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
