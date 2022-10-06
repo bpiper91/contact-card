cf5a72223631c19c993bca95f6a1375edc21b67e
bpiper91 <bpiper91@gmail.com>
2022-10-06 17:42:42 -0400

    swapped GenerateSW service worker for InjectManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -39,28 +40,32 @@ module.exports = {
             template: './index.html',
             title: 'Webpack Plugin'
         }),
-        new WorkboxPlugin.GenerateSW({
-            // Do not precache images
-            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-
-            // Define runtime caching rules
-            runtimeCaching: [{
-                // Match any request that ends with .png, .jpg, .jpeg or .svg.
-                urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-                // Apply a cache-first strategy.
-                handler: 'CacheFirst',
-
-                options: {
-                    // Use a custom cache name.
-                    cacheName: 'images',
-
-                    // Only cache 1 images
-                    expiration: {
-                        maxEntries: 1,
-                    }
-                }
-            }]
+        new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'service-worker.js',
         })
+        // new WorkboxPlugin.GenerateSW({
+        //     // Do not precache images
+        //     exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+        //     // Define runtime caching rules
+        //     runtimeCaching: [{
+        //         // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        //         urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        //         // Apply a cache-first strategy.
+        //         handler: 'CacheFirst',
+
+        //         options: {
+        //             // Use a custom cache name.
+        //             cacheName: 'images',
+
+        //             // Only cache 1 images
+        //             expiration: {
+        //                 maxEntries: 1,
+        //             }
+        //         }
+        //     }]
+        // })
     ]
 };
